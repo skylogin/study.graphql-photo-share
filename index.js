681a45bb0f95cd143ab8480539fa9445fb7a0e73
skylogin <skylogin@gmail.com>
2021-08-27 14:49:50 +0900

    파일 리팩토링 및 express로 교체

--- index.js
+++ index.js
@@ -1,85 +1,34 @@
-const { GraphQLScalarType } = require('graphql');
 const { readFileSync } = require('fs')
-const { ApolloServer } = require('apollo-server');
+const { ApolloServer } = require('apollo-server-express');
+const express = require('express');
+const expressPlayground = require('graphql-playground-middleware-express').default;
 
-var typeDefs = readFileSync('./typeDefs.graphql', 'UTF-8')
+
+
+// graphql 파일분리
+const typeDefs = readFileSync('./typeDefs.graphql', 'UTF-8')
+const resolvers = require('./resolvers');
 
 //테스트 데이터
 var { users, photos, tags } = require('./testData');
 
 
 
-var _id = 4;
-// var photos = [];
+async function app() {
+  var app = express();
+  
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers
+  });
+  
+  await server.start();
+  server.applyMiddleware({ app });
+  
+  app.get('/', (req, res) => res.end('PhotoShare API에 오신걸 환영합니다.'));
+  app.get('/playground', expressPlayground({ endpoint: '/graphql' }));
+  
+  app.listen({ port: 4000 }, () => console.log(`GraphQL Service running on http:/localhost:4000${server.graphqlPath}`));
+}
 
-
-const resolvers = {
-  Query: {
-    totalPhotos: () => photos.length,
-    allPhotos: (parent, args) => photos,
-  },
-
-  Mutation: {
-    postPhoto(parent, args){
-      var newPhoto = {
-        id: _id++,
-        ...args.input,
-        created: new Date()
-      };
-
-      photos.push(newPhoto);
-      return newPhoto;
-    }
-  },
-
-  // trivial resolver
-  Photo: {
-    url: parent => `http://github.com/skylogin/img/${parent.id}.jpg`,
-    postedBy: parent => {
-      return users.find(u => u.githubLogin === parent.githubUser);
-    },
-    taggedUsers: parent => {
-      return tags
-        //현재사진에 대한 태그만 필터링
-        .filter(tag => tag.photoId === parent.id)
-        //태그 배열을 userId배열로 변환
-        .map(tag => tag.userId)
-        //userId배열을 바탕으로 사용자 찾기
-        .map(userId => users.find(u => u.githubLogin === userId));
-    },
-  },
-  User: {
-    postedPhotos: parent => {
-      return photos.filter(p => p.githubUser === parent.githubLogin);
-    },
-    inPhotos: parent => {
-      return tags
-        //현재 사용자에 대한 태그만 필터링
-        .filter(tag => tag.userId === parent.id)
-        //태그 배열을 photoId배열로 변환
-        .map(tag => tag.photoId)
-        //photoId배열을 바탕으로 사진 찾기
-        .map(photoId => photos.find(p=> p.id === photoId));
-    }
-  },
-  DateTime: new GraphQLScalarType({
-    name: 'DateTime',
-    description: 'A valid date time value.',
-    parseValue: value => new Date(value),
-    serialize: value => new Date(value).toISOString(),
-    parseLiteral: ast => ast.value
-  })
-};
-
-
-
-const server = new ApolloServer({
-  typeDefs,
-  resolvers
-});
-
-
-server
-  .listen()
-  .then( ({url}) => console.log(`GraphQL Service running on ${url}`))
-;
+app();

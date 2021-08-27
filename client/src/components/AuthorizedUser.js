130549598d0813e53800102dbafe3a0a43449693
skylogin <skylogin@gmail.com>
2021-08-27 22:35:50 +0900

    cache부분 -- apollo-client로 업데이트되며 몇가지 기능이 되지않음

--- client/src/components/AuthorizedUser.js
+++ client/src/components/AuthorizedUser.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
-import { Mutation } from 'react-apollo';
+import { Mutation, withApollo } from 'react-apollo';
+import {flowRight as compose} from 'lodash';
 
 import { ROOT_QUERY, GITHUB_AUTH_MUTATION } from '../graphql';
 
 import Me from './Me';
 
-const AuthorizedUser = ({ location, match, history }) => {
+const AuthorizedUser = ({ history, client }) => {
   const [signingIn, setSigningIn] = useState(false);
   var githubAuthMutation;
 
@@ -30,6 +31,13 @@ const AuthorizedUser = ({ location, match, history }) => {
     setSigningIn(false);
   }
 
+  function logout(){
+    localStorage.removeItem('token');
+    let data = client.query({ query: ROOT_QUERY });
+    data.me = null;
+    client.writeQuery({ query: ROOT_QUERY, data });
+  }
+
   return (
     <Mutation mutation={GITHUB_AUTH_MUTATION}
       update={authorizationComplete}
@@ -40,7 +48,7 @@ const AuthorizedUser = ({ location, match, history }) => {
         return (
           <Me signingIn={signingIn}
             requestCode={requestCode}
-            logout={() => localStorage.removeItem('token')}
+            logout={logout}
           />
           
         )
@@ -50,4 +58,4 @@ const AuthorizedUser = ({ location, match, history }) => {
 
 };
 
-export default withRouter(AuthorizedUser);
+export default compose(withApollo, withRouter)(AuthorizedUser);
--- client/src/components/Users.js
+++ client/src/components/Users.js
@@ -4,18 +4,33 @@ import { Query, Mutation } from 'react-apollo';
 import { ROOT_QUERY, ADD_FAKE_USERS_MUTATION } from '../graphql';
 
 const Users = () =>
-  <Query query={ROOT_QUERY} pollInterval={8000}>
+  <Query query={ROOT_QUERY} fetchPolicy="cache-and-network">
     {({ data, loading, refetch }) => loading? <p>사용자 불러오는 중...</p>:
       <UserList count={data.totalUsers} users={data.allUsers} refetchUsers={refetch} />
     }
   </Query>
 ;
 
+const updateUserCache = (cache, { data: { addFakeUsers } }) => {
+  let data = cache.readQuery({ query: ROOT_QUERY });
+
+  data.totalUsers += addFakeUsers.length;
+  data.allUsers = [
+    ...data.allUsers,
+    ...addFakeUsers,
+  ];
+
+  // 캐시에 다시 쓰는건데, 이후로 캐시에서 다시 불러들이는 update가 안됨
+  // apollo가 apollo client로 변경되면서 api가 삭제된 모양
+  cache.writeQuery({ query: ROOT_QUERY, data });
+  
+}
+
 const UserList = ({ count, users, refetchUsers }) =>
   <div>
     <p>{count} Users</p>
     <button onClick={() => refetchUsers()}>다시가져오기</button>
-    <Mutation mutation={ADD_FAKE_USERS_MUTATION} variables={{ count: 1}} refetchQueries={[{ query: ROOT_QUERY }]}>
+    <Mutation mutation={ADD_FAKE_USERS_MUTATION} variables={{ count: 1}} update={updateUserCache}>
       {
         addFakeUsers => <button onClick={addFakeUsers}>임시 사용자 추가</button>
       }

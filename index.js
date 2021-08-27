4bcbf90a8010209667a4e9cd05bdb4c9f7061201
skylogin <skylogin@gmail.com>
2021-08-27 12:09:02 +0900

    mutation추가 및 trivial resolver객체 photo-url 추가

--- index.js
+++ index.js
@@ -1,14 +1,47 @@
 const { ApolloServer } = require('apollo-server');
 
 const typeDefs = `
+  type Photo {
+    id: ID!
+    url: String!
+    name: String!
+    description: String
+  }
+
   type Query {
     totalPhotos: Int!
+    allPhotos: [Photo!]!
+  }
+
+  type Mutation {
+    postPhoto(name: String! description: String): Photo!
   }
 `;
 
+
+var _id = 0;
+var photos = [];
+
 const resolvers = {
   Query: {
-    totalPhotos: () => 42
+    totalPhotos: () => photos.length,
+    allPhotos: () => photos,
+  },
+
+  Mutation: {
+    postPhoto(parent, args){
+      var newPhoto = {
+        id: _id++,
+        ...args,
+      };
+
+      photos.push(newPhoto);
+      return newPhoto;
+    }
+  },
+
+  Photo: {
+    url: parent => `http://github.com/skylogin/img/${parent.id}.jpg`
   }
 };
 

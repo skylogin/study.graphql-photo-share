5177fa006b60aa361b629d6e2cf78f5d3aa905f3
skylogin <skylogin@gmail.com>
2021-08-27 17:53:07 +0900

    header내 토큰값 확인추가 및 me mutation추가

--- index.js
+++ index.js
@@ -36,7 +36,12 @@ async function app() {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context
+    context: async ({ req }) => {
+      const githubToken = req.headers.authorization;
+      const currentUser = await db.collection('users').findOne({ githubToken });
+
+      return { db, currentUser };
+    }
   });
   
   await server.start();
--- resolvers/Query.js
+++ resolvers/Query.js
@@ -3,4 +3,5 @@ module.exports = {
   allPhotos: (parent, args, { db }) => db.collection('photos').find().toArray(),
   totalUsers: (parent, args, { db }) => db.collection('users').estimatedDocumentCount(),
   allUsers: (parent, args, { db }) => db.collection('users').find().toArray(),
+  me: (parent, args, { currentUser }) => currentUser,
 };

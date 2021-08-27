ffa9d21882a6e7c78a260e994821af163f87447a
skylogin <skylogin@gmail.com>
2021-08-27 19:15:43 +0900

    클라이언트 유저리스트 출력

--- client/src/index.js
+++ client/src/index.js
@@ -1,49 +1,16 @@
 
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { request } from 'graphql-request';
+import { render } from 'react-dom';
+import { ApolloProvider } from 'react-apollo';
+import ApolloClient from 'apollo-boost';
 
-var url = 'http://localhost:4000/graphql';
+import App from './App';
 
-var query = `
-  query listUsers {
-    allUsers {
-      avatar
-      name
-    }
-  }
-`;
+const client = new ApolloClient({ uri: 'http://localhost:4000/graphql'})
 
-var mutation = `
-  mutation populate($count: Int!){
-    addFakeUsers(count: $count){
-      id
-      name
-    }
-  }
-`;
-
-
-const App = ({ users=[] }) =>
-  <div>
-    {users.map(user =>
-      <div key={user.githubLogin}>
-        <img src={user.avatar} alt="" />
-      </div>
-    )}
-    <button>Add User</button>
-  </div>
-;
-
-const render = ({ allUsers=[] }) =>
-  ReactDOM.render(
-    <App users={allUsers} />, document.getElementById('root')
-  )
-
-
-const requestAndRender = () =>
-  request(url, query)
-    .then(render)
-    .catch(console.error);
-
-requestAndRender();
+render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
